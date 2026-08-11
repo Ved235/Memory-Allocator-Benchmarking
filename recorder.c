a11b96ebf90ad45e11d9cb7cf1dbe03e27991fe1
Ved235 <vedantsinghal07@gmail.com>
2026-08-11 17:04:31 +0530

    Basic malloc intercepter

---
+++ recorder.c
@@ -0,0 +1,33 @@
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdatomic.h>
+#include <dlfcn.h>
+#include <unistd.h>
+
+static void* (*real_malloc)(size_t) = NULL;
+static _Atomic unsigned long mallocCount = 0;
+static __thread int insideHook = 0;
+
+void* malloc(size_t s) {
+    if (insideHook) {
+        const char msg[] = "RECURSIVE malloc\n";
+        write(STDERR_FILENO, msg, sizeof(msg) - 1);
+        return real_malloc(s);
+    }
+
+    if (!real_malloc) {
+        real_malloc = dlsym(RTLD_NEXT, "malloc");
+    }
+
+    insideHook = 1;
+
+    void* p = real_malloc(s);
+    unsigned long n = atomic_fetch_add(&mallocCount, 1) + 1;
+    printf("malloc: %lu %zu %p\n", n, s, p);
+
+    insideHook = 0;
+
+    return p;
+}
