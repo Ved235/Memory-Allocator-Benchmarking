25dfc9aec0044d550ec0413201a962a004b29963
Ved235 <vedantsinghal07@gmail.com>
2026-08-13 02:10:56 +0530

    Add calloc and realloc

--- recorder.c
+++ recorder.c
@@ -8,9 +8,14 @@
 
 static void* (*real_malloc)(size_t) = NULL;
 static void (*real_free)(void* p) = NULL;
+static void* (*real_calloc)(size_t, size_t) = NULL;
+static void* (*real_realloc)(void*, size_t) = NULL;
 
 static _Atomic unsigned long mallocCount = 0;
 static _Atomic unsigned long freeCount = 0;
+static _Atomic unsigned long callocCount = 0;
+static _Atomic unsigned long reallocCount = 0;
+
 static __thread int insideHook = 0;
 
 void* malloc(size_t s) {
@@ -36,12 +41,12 @@ void* malloc(size_t s) {
 }
 
 void free(void* p) {
-    if (insideHook || !p) {
-        return real_free(p);
-    }
-
     if (!real_free) {
         real_free = dlsym(RTLD_NEXT, "free");
+    }
+
+    if (insideHook || !p) {
+        return real_free(p);
     }
 
     insideHook = 1;
@@ -53,4 +58,48 @@ void free(void* p) {
     insideHook = 0;
 
     return;
+}
+
+void* calloc(size_t numElements, size_t s) {
+    if (insideHook) {
+        const char msg[] = "RECURSIVE calloc\n";
+        write(STDERR_FILENO, msg, sizeof(msg) - 1);
+        return real_calloc(numElements, s);
+    }
+
+    if (!real_calloc) {
+        real_calloc = dlsym(RTLD_NEXT, "calloc");
+    }
+
+    insideHook = 1;
+
+    void* p = real_calloc(numElements, s);
+    unsigned long n = atomic_fetch_add(&callocCount, 1) + 1;
+    printf("calloc: %lu %zu %p\n", n, s * numElements, p);
+
+    insideHook = 0;
+
+    return p;
+}
+
+void* realloc(void* ptr, size_t s) {
+    if (insideHook) {
+        const char msg[] = "RECURSIVE realloc\n";
+        write(STDERR_FILENO, msg, sizeof(msg) - 1);
+        return real_realloc(ptr, s);
+    }
+
+    if (!real_realloc) {
+        real_realloc = dlsym(RTLD_NEXT, "realloc");
+    }
+
+    insideHook = 1;
+
+    void* p = real_realloc(ptr, s);
+    unsigned long n = atomic_fetch_add(&reallocCount, 1) + 1;
+    printf("realloc: %lu %zu %p %p\n", n, s, p, ptr);
+
+    insideHook = 0;
+
+    return p;
 }
