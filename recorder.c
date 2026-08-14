2c309e17b09aa389d58da5c9c37cd5131eb49f6b
Vedant Singhal <88609765+Ved235@users.noreply.github.com>
2026-08-14 16:19:33 +0530

    Refactor memory allocation logging functions

--- recorder.c
+++ recorder.c
@@ -11,10 +11,10 @@
 #include <fcntl.h>
 #include <stdint.h>
 
-static void* (*real_malloc)(size_t) = NULL;
-static void (*real_free)(void* p) = NULL;
-static void* (*real_calloc)(size_t, size_t) = NULL;
-static void* (*real_realloc)(void*, size_t) = NULL;
+static void *(*real_malloc)(size_t) = NULL;
+static void (*real_free)(void *p) = NULL;
+static void *(*real_calloc)(size_t, size_t) = NULL;
+static void *(*real_realloc)(void *, size_t) = NULL;
 
 static _Atomic unsigned long mallocCount = 0;
 static _Atomic unsigned long freeCount = 0;
@@ -22,26 +22,29 @@ static _Atomic unsigned long callocCount = 0;
 static _Atomic unsigned long reallocCount = 0;
 
 static __thread int insideHook = 0;
+
 static pthread_once_t fileOpen = PTHREAD_ONCE_INIT;
 static int fileFD = -1;
 
-static void init_file(void) {
+static void init_file(void)
+{
     fileFD = open("alloc_log.txt",
                   O_WRONLY | O_CREAT | O_APPEND,
-                  0644
-                 );
+                  0644);
 }
 
-static void open_file(void) {
+static void open_file(void)
+{
     pthread_once(&fileOpen, init_file);
 }
 
-__attribute__((constructor))
-static void preload_init(void) {
+__attribute__((constructor)) static void preload_init(void)
+{
     open_file();
 }
 
-static uint64_t timestamp(void) {
+static uint64_t timestamp(void)
+{
     struct timespec ts;
     clock_gettime(CLOCK_MONOTONIC, &ts);
     return (uint64_t)ts.tv_sec * 1000000000ULL +
@@ -53,15 +56,13 @@ static void log_alloc(size_t size, void *ptr)
     char buf[128];
     long tid = syscall(SYS_gettid);
     int len = snprintf(
-                  buf,
-                  sizeof(buf),
-                  "a %lu %ld %zu %p\n",
-                  timestamp(),
-                  tid,
-                  size,
-                  ptr
-              );
-
+        buf,
+        sizeof(buf),
+        "a %zu %p %ld %lu\n",
+        size,
+        ptr,
+        tid,
+        timestamp());
     if (len > 0)
         write(fileFD, buf, (size_t)len);
 }
@@ -71,102 +72,128 @@ static void log_free(void *ptr)
     char buf[128];
     long tid = syscall(SYS_gettid);
     int len = snprintf(
-                  buf,
-                  sizeof(buf),
-                  "f %lu %ld %p\n",
-                  timestamp(),
-                  tid,
-                  ptr
-              );
-
+        buf,
+        sizeof(buf),
+        "f %p %ld %lu\n",
+        ptr,
+        tid,
+        timestamp());
     if (len > 0)
         write(fileFD, buf, (size_t)len);
 }
 
-void* malloc(size_t s) {
-    if (insideHook) {
+static void log_realloc(size_t size, void *new_ptr, void *old_ptr)
+{
+    char buf[128];
+    long tid = syscall(SYS_gettid);
+    int len = snprintf(
+        buf,
+        sizeof(buf),
+        "r %zu %p %p %ld %lu\n",
+        size,
+        new_ptr,
+        old_ptr,
+        tid,
+        timestamp());
+    if (len > 0)
+        write(fileFD, buf, (size_t)len);
+}
+
+static void log_calloc(size_t size, size_t numElements, void *ptr)
+{
+    char buf[128];
+    long tid = syscall(SYS_gettid);
+    int len = snprintf(
+        buf,
+        sizeof(buf),
+        "c %zu %zu %p %ld %lu\n",
+        size,
+        numElements,
+        ptr,
+        tid,
+        timestamp());
+    if (len > 0)
+        write(fileFD, buf, (size_t)len);
+}
+
+void *malloc(size_t s)
+{
+    if (insideHook)
+    {
         const char msg[] = "RECURSIVE malloc\n";
         write(STDERR_FILENO, msg, sizeof(msg) - 1);
         return real_malloc(s);
     }
-
-    if (!real_malloc) {
+    if (!real_malloc)
+    {
         real_malloc = dlsym(RTLD_NEXT, "malloc");
     }
-
     insideHook = 1;
-
     open_file();
-    void* p = real_malloc(s);
+    void *p = real_malloc(s);
     mallocCount++;
     log_alloc(s, p);
-
     insideHook = 0;
-
     return p;
 }
 
-void free(void* p) {
-    if (!real_free) {
+void free(void *p)
+{
+    if (!real_free)
+    {
         real_free = dlsym(RTLD_NEXT, "free");
     }
-
-    if (insideHook || !p) {
+    if (insideHook || !p)
+    {
         return real_free(p);
     }
-
     insideHook = 1;
-
     open_file();
     real_free(p);
     freeCount++;
     log_free(p);
-
     insideHook = 0;
-
     return;
 }
 
-void* calloc(size_t numElements, size_t s) {
-    if (insideHook) {
+void *calloc(size_t numElements, size_t s)
+{
+    if (insideHook)
+    {
         const char msg[] = "RECURSIVE calloc\n";
         write(STDERR_FILENO, msg, sizeof(msg) - 1);
         return real_calloc(numElements, s);
     }
-
-    if (!real_calloc) {
+    if (!real_calloc)
+    {
         real_calloc = dlsym(RTLD_NEXT, "calloc");
     }
-
     insideHook = 1;
-
-    void* p = real_calloc(numElements, s);
+    open_file();
+    void *p = real_calloc(numElements, s);
     callocCount++;
-    //printf("calloc: %lu %zu %p\n", callocCount, s * numElements, p);
-
+    log_calloc(s, numElements, p);
     insideHook = 0;
-
     return p;
 }
 
-void* realloc(void* ptr, size_t s) {
-    if (insideHook) {
+void *realloc(void *ptr, size_t s)
+{
+    if (insideHook)
+    {
         const char msg[] = "RECURSIVE realloc\n";
         write(STDERR_FILENO, msg, sizeof(msg) - 1);
         return real_realloc(ptr, s);
     }
-
-    if (!real_realloc) {
+    if (!real_realloc)
+    {
         real_realloc = dlsym(RTLD_NEXT, "realloc");
     }
-
     insideHook = 1;
-
-    void* p = real_realloc(ptr, s);
+    open_file();
+    void *p = real_realloc(ptr, s);
     reallocCount++;
-    //printf("realloc: %lu %zu %p %p\n", reallocCount , s, p, ptr);
-
+    log_realloc(s, p, ptr);
     insideHook = 0;
-
     return p;
 }
